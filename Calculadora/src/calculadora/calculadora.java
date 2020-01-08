6204dc39fc09e6983dc9f0bbc0ad5495dbc2088f
Arroshi <59584423+Arroshi@users.noreply.github.com>
2020-01-08 11:23:45 -0500

    mi primer commit

---
+++ Calculadora/src/calculadora/calculadora.java
@@ -0,0 +1,21 @@
+package calculadora;
+
+public class calculadora {
+	
+	public static double suma(double a, double b) {
+		return a+b;
+	}
+	
+	public static double resta(double a, double b) {
+		return a-b;
+	}
+	
+	public static double division(double a, double b) {
+		return a/b;
+	}
+	public static int multiplicacion(int a, int b ) {
+		return a*b;
+	}
+	
+
+}
---
+++ Calculadora/src/calculadora/calculadoratest.java
@@ -0,0 +1,16 @@
+package calculadora;
+
+public class calculadoratest {
+
+public void testCalculadora() {
+		
+		double resultado1 = calculadora.suma(3, 2);
+		int resultado2 = calculadora.multiplicacion(10, 2);
+		double resultado3 = calculadora.division(11, 2);
+		
+		System.out.println(resultado1);
+		System.out.println(resultado2);
+		System.out.println(resultado3);
+		
+	}
+}
